8d26844a89fcf3201a2e40a1845a655cc20afdb4
IssAjax <96733186+IssAjax@users.noreply.github.com>
2022-11-30 17:29:27 -0500

    Update copying_files.py

--- copying_files.py
+++ copying_files.py
@@ -18,17 +18,27 @@ source_dir = cwd + r"\Network Liaison stuff" + "\\"
 
 # This beginning for kind of made the files a list of lists and had to add another for loop to take the file out of the list 
 # The prints were so that i can see when the file was taken out of the list's 
-
+amount = 0
 for path, dir, files in os.walk(target_dir):
     print(files)
-    for file in files:
-        print(file)
 
-        # Used try to test it on the single file before attempting a mass merge of files. 
-        try:
-            os.rename(path + "\\" + file, source_dir + file)
-            print(f"The move was susccessful {files} now exist in {source_dir}")
+    # As a fail safe this asks if you're sure you want the files to be transferred over.
+    answer = input("Knowing the files in this location are you sure you want to move them? ")
+    if answer.lower() == "yes":
+
+        for file in files:
+
+            #This will count the files in the directory 
+            if os.listdir(target_dir):
+                amount = amount + 1
+            print(amount)
+
+            # Used try to test it on the single file before attempting a mass merge of files. 
+            try:
+                os.rename(path + "\\" + file, source_dir + file)
+                print(f"files were moved susccessful. {amount} files were transferred to {source_dir}.")
     
-        except:
-            print("The move has failed")
-    
+            except:
+                print("The move has failed")
+    else:
+        print("exiting movement script...")
